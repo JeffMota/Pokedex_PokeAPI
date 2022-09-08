21f4c7c29660a1a1f77bb4f2b14af5333af5c3b2
Jefferson Mota <jeffersonmota@pop-os.localdomain>
2022-09-08 20:14:36 -0300

    Tipo do pokémon

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const pokemonNumber = document.querySelector('.pokemon-number');
 const pokemonName = document.querySelector('.pokemon-name');
 const imgBackPokemon = document.querySelector('.img-back-pokemon');
 const imgPokemon = document.querySelector('.img-pokemon');
+const types = document.querySelector('.types');
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.input-form');
@@ -18,12 +19,28 @@ const fetchPokemon = async (pokemon) => {
 //Pokemon info render
 const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
+    const type = data.types;
 
+    //Name and Number
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
+    //Image Pokémon
     imgBackPokemon.src = data['sprites']['versions']['generation-v']['black-white']['animated']['back_default'];
     imgPokemon.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    //Types
+    switch(type.length){
+        case 1:
+            console.log('1');
+            types.innerHTML = `<span class="type-1">${type['0']['type']['name']}</span>`
+            break;
+        case 2:
+            console.log('2');
+            types.innerHTML = `<span class="type-1">${type['0']['type']['name']}</span>
+                                <span class="type-1">${type['1']['type']['name']}</span>`
+            break;
+    }
     
+    input.value = '';
 }
 
 //
