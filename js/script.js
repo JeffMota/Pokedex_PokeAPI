a19a4ba7d01082da5e740be0cbeb65a6ee13b73e
Jefferson Mota <jeffersonmota@pop-os.localdomain>
2022-09-08 12:22:10 -0300

    JavaScript

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const pokemonNumber = document.querySelector('.pokemon-number');
+const pokemonName = document.querySelector('.pokemon-name');
+const imgBackPokemon = document.querySelector('.img-back-pokemon');
+const imgPokemon = document.querySelector('.img-pokemon');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input-form');
+
+//Search pokemon
+const fetchPokemon = async (pokemon) => {
+    const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    const data = await APIresponse.json();
+
+    return data;
+}
+
+//Pokemon info render
+const renderPokemon = async (pokemon) => {
+    const data = await fetchPokemon(pokemon);
+
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    imgBackPokemon.src = data['sprites']['versions']['generation-v']['black-white']['animated']['back_default'];
+    imgPokemon.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    
+}
+
+//
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+});
+
+renderPokemon('2')
